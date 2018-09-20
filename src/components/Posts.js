90d182f958b7c10d99730e6a2ecad9e64a41bf05
mchardex <bukunmiadebisi@gmail.com>
2018-09-20 10:13:48 +0100

    removed unused file

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,44 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {fetchposts} from '../actions/postApiActionCreators'
-import {fetchcomments} from '../actions/commentApiActionCreators'
-import '../Assets/stylesheets/posts.css'
+import { fetchposts } from '../actions/postApiActionCreators'
+import { fetchcomments } from '../actions/commentApiActionCreators'
 import Comments from './Comments'
 
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      comments: []
-    }
-  }
-    
-  componentWillMount() { 
-    this.props.fetchposts()
-    this.props.fetchcomments()
-  }
-
-  render() {
-    const posts = this.props.myApp.posts
-    return (
-      <div>
-        {
-          posts.map(post => {
-            const commentsForPost = this.props.myApp.comments.filter(comment => comment.postId === post.id)
-            return(
-              <div key={post.id} className="shadow-sm p-2 mb-2 bg-white rounded w-50 mx-auto">
-                <button type="button" className="btn btn-outline-none"><h3>{post.title}</h3></button>
-                <Comments comments={commentsForPost} />
-              </div>
-            )
-          })
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+            comments: []
         }
-      </div>
-    )
-  }
+    }
+
+    componentWillMount() {
+        this.props.fetchposts()
+        this.props.fetchcomments()
+    }
+
+    render() {
+        const posts = this.props.myApp.posts
+        return ( <
+            div > {
+                posts.map(post => {
+                    const commentsForPost = this.props.myApp.comments.filter(comment => comment.postId === post.id)
+                    return ( <
+                        div key = { post.id }
+                        className = "shadow-sm p-2 mb-2 bg-white rounded w-50 mx-auto" >
+                        <
+                        button type = "button"
+                        className = "btn btn-outline-none" > < h3 > { post.title } < /h3></button >
+                        <
+                        Comments comments = { commentsForPost }
+                        /> <
+                        /div>
+                    )
+                })
+            } <
+            /div>
+        )
+    }
 }
 
-const mapStateToProps = ({myApp}) => ({myApp})
-export default connect(mapStateToProps, {fetchposts, fetchcomments})(Posts)
+const mapStateToProps = ({ myApp }) => ({ myApp })
+export default connect(mapStateToProps, { fetchposts, fetchcomments })(Posts)
